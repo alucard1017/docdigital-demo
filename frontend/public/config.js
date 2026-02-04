a68fea76eaa30460411df0a81dff966d5ed3f256
alucard1017 <chuquid2000@gmail.com>
2026-02-04 00:14:32 -0500

    Hardcodear API_BASE_URL para Render

--- frontend/public/config.js
+++ frontend/public/config.js
@@ -1,6 +1,4 @@
 // public/config.js
-// Para producción en Render: usar https://firma-express-1.onrender.com
-// Para desarrollo local: usar http://localhost:4000
 window.__APP_CONFIG__ = {
   API_BASE_URL: "https://firma-express-1.onrender.com"
 };
